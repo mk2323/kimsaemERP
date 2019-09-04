fa14ac7e2312ea61441e15e74854adf28f133e7a
Admin <Admin@Admin-PC>
2019-09-04 15:43:00 +0900

    0904_사용자정의로그인 페이지 권한설정(ver2)

--- kimsaemERP/src/main/java/ktds/kimsaem/erp/IndexController.java
+++ kimsaemERP/src/main/java/ktds/kimsaem/erp/IndexController.java
@@ -37,7 +37,7 @@ public class IndexController{
 		System.out.println("인사관리");
 		return "menu/insa";
 	}
-	@RequestMapping(value="/emp/login.do",method=RequestMethod.GET)
+	@RequestMapping(value="/emp/login",method=RequestMethod.GET)
 	public String showadvancedMenu(){
 		System.out.println("login화면");
 		return "login";
